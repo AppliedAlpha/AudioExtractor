770c7bb1d01f5a2a555c4d224487b23671900173
Aria <appliedalpha@naver.com>
2020-09-24 19:16:35 +0900

    Init + Path Loading

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Win32;
+using Xabe.FFmpeg;
+
+namespace AudioExtractor
+{
+    public partial class MainWindow
+    {
+        private FFmpegClass _ffmpegClass;
+        private string _filePath;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _ffmpegClass = new FFmpegClass();
+            _filePath = string.Empty;
+        }
+
+        private void LoadButtonClicked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog {
+                Filter = "All files (*.*)|*.*", 
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _filePath = openFileDialog.FileName;
+                MessageBox.Show(_filePath);
+            }
+        }
+
+        private void ConvertButtonClicked(object sender, System.Windows.RoutedEventArgs e)
+        {
+        }
+    }
+
+    public class FFmpegClass
+    {
+        public FFmpegClass()
+        {
+            
+        }
+    }
+}
